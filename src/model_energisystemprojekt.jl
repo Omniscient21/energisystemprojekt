391dd77a6dd0c96c3ce079412b3eb361a5bd9cf5
Omniscient21 <anestrandalvin@gmail.com>
2022-04-13 16:13:47 +0200

    Fixed Systemcost constraint!

--- src/model_energisystemprojekt.jl
+++ src/model_energisystemprojekt.jl
@@ -14,45 +14,54 @@ function buildmodel(input)
         Electricity[r in REGION, p in PLANT, h in HOUR]       >= 0        # MWh/h
         Capacity[r in REGION, p in PLANT]                     >= 0        # MW
     end #variables
-
+    #print(Capacity)
 
     #Variable bounds
     for r in REGION, p in PLANT
         set_upper_bound(Capacity[r, p], maxcap[r, p])
     end
 
-    # Defining costs of the system
-    Runningcost = AxisArray(Array{Float64}(undef, length(REGION)),REGION) # init
-    Investmentcost = AxisArray(Array{Float64}(undef, length(REGION)),REGION) # init
-    Systemcost = AxisArray(Array{Float64}(undef, length(REGION)),REGION) # init
-
     function a(d,p)
         1-(1/(1+d)^assum[:Lifetime,p])
     end
-    for r in REGION
-        #print(Capacity[r,:Wind].*assum[:InvestmentCost,:Wind].*discountrate./a(discountrate,:Wind))
 
-        Investmentcost[r] = sum(Capacity[r,p] for p in PLANT) # doesn't work, wrong type
+    # test expression TODO: control all expressions
+    @expression(m, Runningcost[r = REGION], sum(Electricity[r,p,h].*assum[:RunCost,p] for p in PLANT, h in HOUR))
+    @expression(m, Investmentcost[r = REGION], sum(Capacity[r,p].*assum[:InvestmentCost,p].*discountrate./a(discountrate,p) for p in PLANT))
+    @expression(m, Fuelcost[r = REGION], sum(Electricity[r,:Gas,h].*assum[:FuelCost,:Gas] for h in HOUR))
+    @expression(m, Systemcost[r = REGION], Runningcost[r] + Investmentcost[r] + Fuelcost[r])
 
-        Investmentcost[r] = sum(Capacity[r,p].*assum[:InvestmentCost,p].*discountrate./a(discountrate,p) for p in PLANT)
-        Fuelcost[r] = sum(Electricity[r,Gas,h].*assum[:FuelCost,:Gas] for h in HOUR)
-    end
-    for r in REGION # when r used many times external for loop needed
-        print(Electricity[:DE,:Wind,100])
 
-        # seems to not work as Electricity[r,p,h] is not a normal float element
-        Runningcost[r] = sum(Electricity[r,p,h].*assum[:RunCost,p] for p in PLANT, h in HOUR)
-    end
-    for r in REGION
-        Systemcost[r] = Runningcost[r] + Investmentcost[r] + Fuelcost[r]
-    end
+    # Defining costs of the system
+    # Runningcost = AxisArray(Array{Float64}(undef, length(REGION)),REGION) # init
+    # Investmentcost = AxisArray(Array{Float64}(undef, length(REGION)),REGION) # init
+    # Systemcost = AxisArray(Array{Float64}(undef, length(REGION)),REGION) # init
+    #
+    # for r in REGION
+    #     #print(Capacity[r,:Wind].*assum[:InvestmentCost,:Wind].*discountrate./a(discountrate,:Wind))
+    #
+    #     #Investmentcost[r] = sum(hej[r,p] for p in PLANT) # doesn't work, wrong type
+    #
+    #     Investmentcost[r] = sum(Capacity[r,p].*assum[:InvestmentCost,p].*discountrate./a(discountrate,p) for p in PLANT)
+    #     Fuelcost[r] = sum(Electricity[r,Gas,h].*assum[:FuelCost,:Gas] for h in HOUR)
+    # end
 
+    # for r in REGION # when r used many times external for loop needed
+    #     print(Electricity[:DE,:Wind,100])
+    #
+    #     # seems to not work as Electricity[r,p,h] is not a normal float element
+    #     Runningcost[r] = sum(Electricity[r,p,h].*assum[:RunCost,p] for p in PLANT, h in HOUR)
+    # end
+
+    # for r in REGION
+    #     Systemcost[r] = Runningcost[r] + Investmentcost[r] + Fuelcost[r]
+    # end
 
     @constraints m begin
-        Generation[r in REGION, p in PLANT, h in HOUR],
+        Generation[r in REGION, p in PLANT, h in HOUR], # name of constraint?
             Electricity[r, p, h] <= Capacity[r, p] # * capacity factor
 
-        Systemcost[r in REGION],
+        Syscost[r in REGION], # name of constraint?
             Systemcost[r] >= 0 # sum of all annualized costs
 
     end #constraints
